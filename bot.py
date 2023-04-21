383e6d898b7f971feb52bf493839607fb234bca8
TillyC <t.cullen0720181@arts.ac.uk>
2023-04-21 12:52:12 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,46 @@
+import openai
+import discord
+import os
+
+openai.api_type = "azure"
+openai.api_version = "2023-03-15-preview"
+
+# specifying our server
+GUILD = "{Tilly's-Server}"
+
+# create an object that will control our discord bot
+client = discord.Client(intents=discord.Intents.default())
+openai.api_key = os.environ["API_KEY"]
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+openai.api_base = os.environ["API_BASE"]
+#get our bot online
+#guild is another term for server
+@client.event
+async def on_ready():
+	for guild in client.guilds:
+		if guild.name == GUILD:
+			break
+	# print out nice statment saying our bot is online (only in command prompt)
+	print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+	# this prevents inifinte loops of bot talking to bot
+	# if author of the message is the bot, don't do anything
+	if message.author == client.user:
+		return
+	# ignore @everyone mentions
+	if message.mention_everyone:
+		return
+	# if the message mentions the bot, then do something
+	elif client.user.mentioned_in(message): 
+		response = openai.ChatCompletion.create(
+			engine="GPT-4",
+			messages=[
+			{"role": "system", "content": "You are an LED electronics component that is child friendly and kind. You exist to help teach people about electronic circuits. Include emojis in your answers. Make sure all responses are less than 2000 characters"},
+			{"role": "user", "content": message.content}
+			]
+		)
+		await message.channel.send(response.choices[0].message.content)
+
+client.run(DISCORD_TOKEN)
